3107bcf0b54243e798ed784cd7b4eadad81b0590
Thiru <114560047+Thiruop@users.noreply.github.com>
2025-04-28 12:25:16 +0530

    Update ReviewFilter.js

--- Routes/ReviewFilter.js
+++ Routes/ReviewFilter.js
@@ -2,5 +2,5 @@ import express from "express";
 import {GetCompany, CompanyFilter} from "../Controller/Filter.js";
 const router = express.Router();
 router.get("/company", GetCompany);
-router.get("/company/filter", CompanyFilter);
+router.post("/company/filter", CompanyFilter);
 export default router;
